bb0f1f1dad146544d590f7f6a3d5ef07bbaef628
AGhaith <a.wesam2300@nu.edu.eg>
2025-01-08 00:13:17 +0200

    add

--- BST.h
+++ BST.h
@@ -1,40 +1,10 @@
-#ifndef SYSTEMHEADER_H
-#define SYSTEMHEADER_H
+
+#ifndef BST_H
+#define BST_H
 
 #include <iostream>
 #include <string>
 using namespace std;
-
-class Course {
-private:
-    string CourseID;
-    string CourseName, CourseInstructor;
-    int CourseCredits;
-
-public:
-    Course(string ID = "", string name = "", string instructor = "", int credits = 0) {
-        CourseID = ID;
-        CourseName = name;
-        CourseInstructor = instructor;
-        CourseCredits = credits;
-    }
-
-    string get_id() {
-        return CourseID;
-    }
-
-    string get_name() {
-        return CourseName;
-    }
-
-    string get_instructor() {
-        return CourseInstructor;
-    }
-
-    int get_credits() {
-        return CourseCredits;
-    }
-};
 
 template <class T>
 class BSTNode {
