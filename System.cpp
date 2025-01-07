3a0defeb33155053d1b8479252564c38d62e5550
AGhaith <a.wesam2300@nu.edu.eg>
2025-01-07 23:43:57 +0200

    Update System.cpp

--- System.cpp
+++ System.cpp
@@ -1,6 +1,40 @@
 #include<iostream>
 #include"SystemHeader.h"
 using namespace std;
+class Course{
+private:
+string CourseID;
+string CourseName,CourseInstructor;
+int CourseCredits;
+public:
+Course(string ID="",string name="",string instructor="",int credits=0){
+CourseID=ID;
+CourseName=name;
+CourseInstructor=instructor;
+CourseCredits=credits;
+}
+string get_id(){
+return CourseID;
+}
+string get_name() {
+    return CourseName;
+}
+
+string get_instructor() {
+    return CourseInstructor;
+}
+
+int get_credits() {
+    return CourseCredits;
+}
+
+};
+template <class T>
+class BSTNode{
+private:
+public:
+
+};
 int main(){
 
 
