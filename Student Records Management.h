f8c590d19efc491fb376f7e6e1db00f9a0d674c6
omar emad <omaremad11113@gmail.com>
2025-01-08 01:40:48 +0200

    1:40 8/1 ;)

--- Student Records Management.h
+++ Student Records Management.h
@@ -9,8 +9,16 @@ using namespace std ;
 
 class Student_Records_Management{
     private:
-    SLL<Student> Students ; 
-    
+    SLL<Student> SLL_Students ; 
+    public:
+    void Add_student(string name , string Email , string Phone ,string Address , string Password){
+
+        Student s1(name , Email , Phone , Address , Password) ; 
+        SLL_Students.insert(s1) ; 
+    }
+    void Delete_Student(int ID){
+        SLL_Students.deleteStudent(ID) ; 
+    }
 };
 
 
